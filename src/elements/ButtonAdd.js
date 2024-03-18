1190708fc4449578a4182d4edd1e20eceb7861e4
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-18 12:44:10 +0700

    add tailwindcss config

--- src/elements/ButtonAdd.js
+++ src/elements/ButtonAdd.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonAdd = () => {
   return (
     <div>
-      <button>Add</button>
+      <button className='OpenCustomOrder bg-blue-700 text-sm text-white font-semibold py-1 px-2 rounded-lg shadow-md w-20 h-7'>Add</button>
     </div>
   )
 }
