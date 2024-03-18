090d5736c77c959a0a9b5dcc7557c47aba7e5f1a
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-18 14:29:04 +0700

    feat homepage

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className=' text-center py-5'>
+      <p className='judul font-semibold text-base pt-8'>Table 2</p>
+      <p className='font-medium text-sm pb-4'>Serenity Outlet Bogor</p>
+    </div>
+  )
+}
+
+export default Title
--- src/pages/Homepages.js
+++ src/pages/Homepages.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Productcategory from '../components/Productcategory'
+import Title from '../components/Title'
+import Search from '../components/Search'
 
 const Homepages = () => {
   return (
-    <div className='max-w-md mx-auto bg-green-100'>
-      Homepages
+    <div className='max-w-md mx-auto bg-green-100 h-screen'>
+      <Title/>
+      <Search/>
       <Productcategory/>
       <Productcategory/>
     </div>
