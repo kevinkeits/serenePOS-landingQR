fe27dcf51181fe06771d9201635ee33a27170dc8
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-19 16:24:38 +0700

    feat orderpages

--- src/pages/Orderpages.js
+++ src/pages/Orderpages.js
@@ -12,9 +12,13 @@ const Orderpages = () => {
   const [showOrderPages, setShowOrderPages] = useState(true);
   const [showCartPages, setShowCartPages] = useState(false);
   const [qty, setQty] = useState(1);
+  const [orderPrice, setOrderPrice] = useState(0);
+  const [ProductPrice, ] = useState(25000);
+
 
   const updateQty = newQty => {
     setQty(newQty);
+    setOrderPrice(newQty * ProductPrice); 
   }
 
   const backtoHome = () => {
@@ -33,14 +37,14 @@ const Orderpages = () => {
      {showOrderPages && (
       <div>
         <Header backtoHome={backtoHome} label={'Customize Order'} />
-        <ProductLabel ProductPrice={'25000'}/>
+        <ProductLabel ProductPrice={ProductPrice.toString()}/>
         <ProductVariant
           VariantType={['Serve', 'Sugar', 'Add On']}
           VariantLabel={[['Ice', 'Hot'], ['Normal', 'Less Sugar', 'More Sugar'], ['Sugar Syrup', 'Bobba', 'Grass Jelly', 'Milk', 'Cheese']]}
           VariantPrice={[['0', '0'], ['0', '1000', '0'], ['2000', '3000', '4000', '5000', '6000']]}
         />
         <ProductNotes />
-        <ProductQty addtocart={AddToCart} onChange={updateQty} value={qty}/>
+        <ProductQty addtocart={AddToCart} onChange={updateQty} value={qty} orderPrice={orderPrice.toString()}/>
       </div>
   )}
   {showProductPages && (<Homepages  />)}
