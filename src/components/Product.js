e8549f74912a5dca0ec09285738f82797bd21c57
Sheelyn <rezaalfarisi3@gmail.com>
2024-03-20 11:57:38 +0700

    fix: using array on homepage

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-const Product = ({ customOrder, itemName, itemDescription, itemPrice, ProductPhoto }) => {
+const Product = ({ customOrder, products}) => {
   return (
-    <div className='productMenu flex mx-2 my-2 py-2 bg-white px-2 border rounded-lg shadow-md'>
-      <div className=''>
-        <img src={ProductPhoto} alt='Matcha Latte' className='w-28 h-24 rounded-md'/>
-      </div>
-      <div className='mx-2 w-full'>
-        <p className='text-base font-semibold '>{itemName}</p>
-        <p className='text-xs'>{itemDescription}</p>
-        <p className='text-md font-medium'>{itemPrice}</p>
-        <div className='text-right'>
-          <div className='py-1'>
-            <button onClick={customOrder} className='bg-blue-700 text-sm text-white font-semibold py-1 px-2 rounded-lg shadow-md w-20'>Add</button>
+    <div>
+      {products.map((product, index) => (
+        <div key={index} className='productMenu flex mx-2 my-2 py-2 bg-white px-2 border rounded-lg shadow-md'>
+          <div>
+            <img src={product.ProductPhoto} alt={product.itemName} className='w-28 h-24 rounded-md'/>
           </div>
+          <div className='mx-2 w-full'>
+            <p className='text-base font-semibold'>{product.itemName}</p>
+            <p className='text-xs'>{product.itemDescription}</p>
+            <p className='text-md font-medium'>{product.itemPrice}</p>
+            <div className='text-right'>
+              <div className='py-1'>
+                <button onClick={() => customOrder(product)} className='bg-blue-700 text-sm text-white font-semibold py-1 px-2 rounded-lg shadow-md w-20'>Add</button>
+              </div>
+            </div>
+          </div>     
         </div>
-      </div>     
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/pages/Homepages.js
+++ src/pages/Homepages.js
@@ -1,20 +1,17 @@
-import React, { useState,  } from 'react'
+import React, { useState } from 'react';
+import Title from '../components/Title';
+import Search from '../components/Search';
+import Orderpages from './Orderpages';
+import Cartpages from './Cartpages';
+import Product from '../components/Product';
 
-import Title from '../components/Title'
-import Search from '../components/Search'
-import Orderpages from './Orderpages'
-import Cartpages from './Cartpages'
-import Product from '../components/Product'
-
-import MatchaLattePic from '../assets/thumbnail/Matcha Latte.png'
-import VelvetLattePic from '../assets/thumbnail/Velvet Latte.png'
-import BaileysCoffeePic from '../assets/thumbnail/Baileys-Coffee.png'
-import AmericanoPic from '../assets/thumbnail/Americano.png'
-import CappucinoPic from '../assets/thumbnail/Cappucino.png'
-import KopiTubrukPic from '../assets/thumbnail/Kopi-Tubruk.png'
-import VietnamDripPic from '../assets/thumbnail/Vietnam-Drip.png'
-
-
+import MatchaLattePic from '../assets/thumbnail/Matcha Latte.png';
+import VelvetLattePic from '../assets/thumbnail/Velvet Latte.png';
+import BaileysCoffeePic from '../assets/thumbnail/Baileys-Coffee.png';
+import AmericanoPic from '../assets/thumbnail/Americano.png';
+import CappucinoPic from '../assets/thumbnail/Cappucino.png';
+import KopiTubrukPic from '../assets/thumbnail/Kopi-Tubruk.png';
+import VietnamDripPic from '../assets/thumbnail/Vietnam-Drip.png';
 
 const Homepages = () => {
   const [showProductPages, setShowProductPages] = useState(true);
@@ -22,41 +19,39 @@ const Homepages = () => {
   const [showCartPages, setShowCartPages] = useState(false);
 
   const openPages = () => {
-    setShowOrderPages(true)
-    setShowProductPages(false)
-    setShowCartPages(false)
-  }
+    setShowOrderPages(true);
+    setShowProductPages(false);
+    setShowCartPages(false);
+  };
+
+  const products = [
+    { itemName: 'Matcha Latte', itemDescription: 'Hot/Cold', itemPrice: 'Rp 28,000', ProductPhoto: MatchaLattePic },
+    { itemName: 'Velvet Latte', itemDescription: 'Hot/Cold', itemPrice: 'Rp 28,000', ProductPhoto: VelvetLattePic },
+    { itemName: 'Americano', itemDescription: 'Hot/Cold', itemPrice: 'Rp 20,000', ProductPhoto: AmericanoPic },
+    { itemName: 'Cappucino', itemDescription: 'Hot/Cold', itemPrice: 'Rp 28,000', ProductPhoto: CappucinoPic },
+    { itemName: 'Baileys Coffee', itemDescription: 'Cold', itemPrice: 'Rp 30,000', ProductPhoto: BaileysCoffeePic },
+    { itemName: 'Kopi Tubruk', itemDescription: 'Hot', itemPrice: 'Rp 20,000', ProductPhoto: KopiTubrukPic },
+    { itemName: 'Vietnam Drip', itemDescription: 'Hot', itemPrice: 'Rp 25,000', ProductPhoto: VietnamDripPic }
+  ];
 
   return (
     <div className='max-w-md mx-auto h-screen'>
-    {showProductPages && (
-     <div>
-        <Title/>
-        <Search/>
-        <div className='my-2'>
-          <div className='categoryMenu max-w-md mx-auto'>
-              <p className='font-bold py-2 px-2'> Non Coffee</p>
-              <Product customOrder={openPages} ProductPhoto={MatchaLattePic} itemName={'Matcha Latte'} itemDescription={'Hot/Cold'} itemPrice={'Rp 28,000'}/>
-              <Product customOrder={openPages} ProductPhoto={VelvetLattePic} itemName={'Velvet Latte'} itemDescription={'Hot/Cold'} itemPrice={'Rp 28,000'} />
-          </div>
+      {showProductPages && (
+        <div>
+          <Title />
+          <Search />
+          <p className='mx-2 text-base font-bold my-2'>Non Coffee</p>
+          <Product customOrder={openPages} products={products.filter(product => !product.itemName.includes('Coffee'))} />
+          <p className='mx-2 text-base font-bold my-2'>Coffee</p>
+          <Product customOrder={openPages} products={products.filter(product => product.itemName.includes('Coffee'))} />
+          <p className='mx-2 text-base font-bold my-2'>Add On</p>
+          <Product customOrder={openPages} products={products.filter(product => product.itemName.includes('Coffee'))} />
         </div>
-        <div className='my-2'>
-          <div className='categoryMenu max-w-md mx-auto'>
-              <p className='font-bold py-2 px-2'> Coffee</p>
-              <Product customOrder={openPages} ProductPhoto={AmericanoPic} itemName={'Americano'} itemDescription={'Hot/Cold'} itemPrice={'Rp 20,000'} />
-              <Product customOrder={openPages} ProductPhoto={CappucinoPic} itemName={'Cappucino'} itemDescription={'Hot/Cold'} itemPrice={'Rp 28,000'} />
-              <Product customOrder={openPages} ProductPhoto={BaileysCoffeePic} itemName={'Baileys Coffee'} itemDescription={'Cold'} itemPrice={'Rp 30,000'} />
-              <Product customOrder={openPages} ProductPhoto={KopiTubrukPic} itemName={'Kopi Tubruk'} itemDescription={'Hot'} itemPrice={'Rp 20,000'} />
-              <Product customOrder={openPages} ProductPhoto={VietnamDripPic} itemName={'Vietnam Drip'} itemDescription={'Hot'} itemPrice={'Rp 25,000'} />
-              
-          </div>
-        </div>
-      </div>
-    )}
-    {showOrderPages && (<Orderpages  />)}
-    {showCartPages && (<Cartpages />)}
+      )}
+      {showOrderPages && <Orderpages />}
+      {showCartPages && <Cartpages />}
     </div>
-  )
-}
+  );
+};
 
-export default Homepages
+export default Homepages;
