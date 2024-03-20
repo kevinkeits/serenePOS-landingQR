62beaa9a9f77af04d367f4e8ea60b7944257396c
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-20 16:04:51 +0700

    update order detail

--- src/components/Orderdetail.js
+++ src/components/Orderdetail.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Inputqty from './Inputqty'
 import Productimage from '../assets/thumbnail/Cappucino.png'
-import iconNotes from '../assets/Icon/note-text-svgrepo-com.svg'
 
-const Orderdetail = ({ UpdateOrderNotes}) => {
+const Orderdetail = ({OrderPrice, OrderNotes, seledtedVarOp}) => {
   return (
-    <div className='px-2 m-2 py-4'>
+    <div className='px-2 m-2 py-4 border rounded-lg'>
       <div className="flex items-center">
         <div className="grow">
           <p className="font-semibold text-lg">Matcha latte</p>
@@ -14,26 +12,18 @@ const Orderdetail = ({ UpdateOrderNotes}) => {
             <label className='Var-Serve-Option'>Ice</label>
           </div>
           <div id="variant" className='inline-flex w-full gap-1'>
-            <p className='font-semibold text-base'>Sugar:</p>
+            <p className='font-semibold text-base'>Sugar: {seledtedVarOp}</p>
             <label className='Var-Serve-Option'>Ice</label>
           </div>
-          <p className="font-semibold">Notes:</p>
+          <p className="font-semibold">Notes: {OrderNotes}</p>
           <div id="variant" className='inline-flex w-full gap-1'>
-            <p className='font-semibold text-base'>Price:</p>
+            <p className='font-semibold text-base'>Price: {OrderPrice}</p>
             <label className='Var-Serve-Option'>30000</label>
           </div>
         </div>
         <img src={Productimage} alt="" class="w-20 h-20 rounded-md"></img>
       </div>
-
-      <div className='flex items-center justify-between py-2'>
-        <div onClick={UpdateOrderNotes} className='addNotes inline-flex border rounded-lg hover:cursor-pointer py-2 shadow-md px-4'>
-          <img src={iconNotes} alt='Add Notes' className='w-6 h-6'/>
-          <p className='font-medium text-base'>Add Notes</p>
-        </div>
-          <Inputqty/>
-        </div> 
-        
+      
 
     </div>
   )
--- src/pages/Cartpages.js
+++ src/pages/Cartpages.js
@@ -7,7 +7,6 @@ const Cartpages = () => {
   const [showProductPages, setShowProductPages] = useState(false);
   const [showOrderPages, setShowOrderPages] = useState(true);
   const [showCartPages, setShowCartPages] = useState(false);
-  const [showOrderNotes, setShowOrderNotes] = useState(false);
 
   const backtoHome = () => {
     setShowProductPages(true)
@@ -15,60 +14,46 @@ const Cartpages = () => {
     setShowCartPages(false)
   }
 
-  const OrderNotes = () => {
-    setShowOrderNotes(true)
-  }
-
-  const SaveOrderNotes = () => {
-    setShowOrderNotes(false)
-  }
-
-  const CloseOrderNotes = () => {
-    setShowOrderNotes(false)
-  } 
-
   return (
     <div>
       {showProductPages && (<Homepages  />)}
       {showCartPages && (<Cartpages />)}
       {showOrderPages && (
-      <div className='max-w-md mx-auto h-screen'>
-        <Header backtoHome={backtoHome} label={'Cart'} />
-        <div>
-          <Orderdetail UpdateOrderNotes={OrderNotes} />
-          <Orderdetail UpdateOrderNotes={OrderNotes} />
-        </div>
-        <div className='max-w-md mx-auto flex items-center justify-between my-2 px-2'>
-          <p font-medium text-base>Need Anything else?</p>
-          <button className='border py-2 px-2 rounded-lg shadow-md mx-2 text-base'>Add More</button>
-        </div>
-        <div className='border max-w-md mx-auto px-2 mt-3 mb-4 rounded-lg'>
-          <p className='font-bold text-lg py-2'>Summary</p>
-          <div className='border-t-2 flex py-2'>
-            <p className='font-bold text-lg grow'>Total</p>
-            <p className='font-bold text-lg'>60000</p>
-            </div>
-        </div>
-        <div className='bg-blue-600 max-w-md mx-auto my-2 rounded-lg px-2 py-3 items-center'>
-          <p className='text-white font-semibold text-lg text-center'>Place Order</p>
-        </div>
-
+        <div className='max-w-md mx-auto h-screen'>
+          <Header backtoHome={backtoHome} label={'Cart'} />
+          <div>
+            <Orderdetail />
+          </div>
+                             
           {/* Notes */}
-          {showOrderNotes && (
-            <div className='modalAddNotes sticky bg-white bottom-0 w-full border rounded-lg mb-2 shadow-lg'>
+          <div className='sticky bg-white w-full rounded-lg mb-2'>
             <div className='py-2 mx-2'>
               <p className='text-base font-semibold'>Notes</p>
             </div>
             <div className='bg-white border shadow-md rounded-md py-2 px-3 my-1 mx-2'>
               <textarea placeholder='Ex: Add more...' className='w-full h-32 border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none' />
-            </div>
-            <div className='flex my-2 mx-2 items-center'>
-              <p className='text-slate-200 grow'>0/200</p>
-              <button onClick={CloseOrderNotes} className='cancelNotes bg-white border rounded-lg shadow-lg text-black text-base p-2 w-20 mx-1'>Cancel</button>
-              <button onClick={SaveOrderNotes} className='saveNotes border rounded-lg bg-green-600 shadow-lg text-white text-base font-medium p-2 w-20 mx-1'>Save</button>
-            </div>
+              <div className='flex my-2 mx-2 items-center'>
+                <p className='text-slate-200 grow'>0/200</p>
+              </div>
+            </div> 
           </div>
-          )}
+
+          <div className='max-w-md mx-auto flex items-center justify-between my-2 px-2'>
+            <p font-medium text-base>Need Anything else?</p>
+            <button className='border py-2 px-2 rounded-lg shadow-md mx-2 text-base'>Add More</button>
+          </div>
+
+          <div className='border max-w-md mx-auto px-2 mt-3 mb-4 rounded-lg'>
+            <p className='font-bold text-lg py-2'>Summary</p>
+            <div className='border-t-2 flex py-2'>
+              <p className='font-bold text-lg grow'>Total</p>
+              <p className='font-bold text-lg'>60000</p>
+              </div>
+          </div>
+
+          <div className='bg-blue-600 max-w-md mx-auto my-2 rounded-lg px-2 py-3 items-center'>
+            <p className='text-white font-semibold text-lg text-center'>Place Order</p>
+          </div>
         </div>
       )}
     </div>
