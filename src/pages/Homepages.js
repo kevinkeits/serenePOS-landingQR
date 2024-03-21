bb0b8b7aea24de12594680198959bbed7f30e49e
Sheelyn <rezaalfarisi3@gmail.com>
2024-03-21 14:56:23 +0700

    fix: fix price using parseFloat, add no image.

--- src/pages/Homepages.js
+++ src/pages/Homepages.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Title from '../components/Title';
-import Search from '../components/Search';
+import Searchicon from '../assets/Icon/search-svgrepo-com.svg'
 import Orderpages from './Orderpages';
 import Cartpages from './Cartpages';
 import Product from '../components/Product';
+import NoImage from '../assets/thumbnail/No-Image-Placeholder.png'
 
 const Homepages = () => {
   const [selectedProduct, setSelectedProduct] = useState(null);
@@ -14,6 +15,7 @@ const Homepages = () => {
   const [showCartPages, setShowCartPages] = useState(false);
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const openPages = (product) => {
     setSelectedProduct(product);
@@ -25,7 +27,11 @@ const Homepages = () => {
   useEffect(() => {
     axios.get('https://serenepos.temandigital.id/api/scanOrder/get')
       .then(response => {
-        const Product = response.data.data;
+        const Product = response.data.data.map(product => ({
+          ...product, 
+          price: parseFloat(product.price).toFixed(0),
+          imgUrl: product.imgUrl ? product.imgUrl : NoImage
+        }));
         setProducts(Product);
 
         const Categories = [...new Set(Product.map(product => product.categoryName))];
@@ -36,16 +42,25 @@ const Homepages = () => {
       });
   }, []);
 
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className='max-w-md mx-auto h-screen'>
       {showProductPages && (
         <div>
           <Title />
-          <Search />
+          <div className=' w-full max-w-md mx-auto py-2 px-2'>
+              <div className='w-full flex items-center py-2 px-2 border bg-white border-gray-300 shadow-md rounded-lg '>
+                <input type='text' placeholder='Cari Disini...' className='pl-2 w-full' value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+              <img src={Searchicon} alt='Search' className='w-6 h-6 mx-2' />
+              </div>
+          </div>
           {categories.map(category => (
             <div key={category}>
               <p className='mx-2 text-base font-bold my-2'>{category}</p>
-              <Product customOrder={openPages} products={products.filter(product => product.categoryName === category)} />
+              <Product customOrder={openPages} products={filteredProducts.filter(product => product.categoryName === category)} />
             </div>
           ))}
         </div>
