c5e66e4c7de499c7236fa30b6b4ab883c22de278
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-21 11:42:44 +0700

    feat orderpages

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import Searchicon from '../assets/Icon/search-svgrepo-com.svg'
 const Search = () => {
   return (
     <div className=' w-full max-w-md mx-auto py-2 px-2'>
-        <div className='w-full flex items-center py-2 px-2 border bg-white border-gray-300 shadow-md rounded-lg focus:ring-gray-300 focus:border-gray-300 focus:ring focus:ring-opacity-50 '>
+        <div className='w-full flex items-center py-2 px-2 border bg-white border-gray-300 shadow-md rounded-lg focus:ring-gray-300 focus:border-gray-300 focus:ring focus:ring-opacity-20 '>
         <input type='text' placeholder='Cari Disini...' className='pl-2 block w-full border-r-2'/>
         <img src={Searchicon} alt='Search' className='w-6 h-6 mx-2' />
         </div>
--- src/pages/Orderpages.js
+++ src/pages/Orderpages.js
@@ -4,13 +4,29 @@ import ProductNotes from '../components/ProductNotes';
 import Header from '../components/Header';
 import Cartpages from './Cartpages';
 import Homepages from './Homepages';
+import axios from 'axios';
 
 const Orderpages = ({ selectedProduct }) => {
   const [showProductPages, setShowProductPages] = useState(false);
   const [showOrderPages, setShowOrderPages] = useState(true);
   const [showCartPages, setShowCartPages] = useState(false);
   const [qty, setQty] = useState(1);
+  const [data, setData] = useState([]);
   const [orderPrice, setOrderPrice] = useState(selectedProduct ? selectedProduct.price : 0);
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://serenepos.temandigital.id/api/scanOrder/get');
+      const jsonData = await response.json();
+      setData(jsonData); // Menyimpan data ke dalam state
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (selectedProduct && !isNaN(qty) && !isNaN(selectedProduct.price)) {
@@ -61,11 +77,28 @@ const Orderpages = ({ selectedProduct }) => {
                 <p id="itemPrice" className="text-sm font-medium pl-1">Rp. {price}</p>
             </div> 
           </div> 
-          <ProductVariant
+
+         {/*  Memanggil data variant */}
+          {/* {data.map(item => ())} */}
+
+          {/* <ProductVariant
             VariantType={['Serve', 'Sugar', 'Add On']}
+            
             VariantLabel={[['Ice', 'Hot'], ['Normal', 'Less Sugar', 'More Sugar'], ['Sugar Syrup', 'Bobba', 'Grass Jelly', 'Milk', 'Cheese']]}
             VariantPrice={[['0', '0'], ['0', '1000', '0'], ['2000', '3000', '4000', '5000', '6000']]}
-          />
+          /> */}
+
+          <div className='px-2 m-2'>
+            <p className="font-semibold">Matchaa</p>
+            <div className="flex gap-2 items-center space-x-2 py-1">
+              <input className='focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300' type="radio" id='123' name='aaa' value='0' />
+              <label className="ml-2 font-medium text-base text-gray-700 w-24">000</label>
+                <div className="grow bg-slate-200 rounded-md px-2 py-1">
+                  <span className="ml-2 text-gray-500">000</span>
+                </div>
+            </div>
+          </div>
+
           <ProductNotes />
           <div className="max-w-md mx-auto border-t-2 pt-4 pb-3 px-3">
             <div className="flex gap-2">
